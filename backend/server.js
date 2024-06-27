6f45ea51ca4f476d896b69ac28c6d752a05811ca
patel-het-github <het7355@gmail.com>
2024-06-27 11:22:19 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,15 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-dotenv.config();
 
-const __dirname = path.resolve();
+
+
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -28,6 +32,7 @@ app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
+
 
 server.listen(PORT, () => {
 	connectToMongoDB();
